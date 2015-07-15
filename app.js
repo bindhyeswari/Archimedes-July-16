f0f6c8b1b001505116f57dc345077c7b26272bd6
mishra <tech@golivelabs.io>
2015-07-15 12:40:57 -0700

    get and post for contacts

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var contacts = require('./routes/contacts');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/contacts', contacts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/contacts.js
@@ -0,0 +1,26 @@
+/**
+ * Created by mishrab on 7/15/15.
+ */
+
+var router = require('express').Router();
+var uuid = require('uuid');
+
+console.log(uuid.v4());
+
+var contacts = [];
+
+// this callback is executed for all get requests to /contacts
+router.get('/', function (req, res) {
+    res.status(200).json(contacts)
+});
+
+router.post('/', function (req, res) {
+    req.body._id = uuid.v4();
+    contacts.push(req.body);
+    res.status(200).json({
+        message: 'Successfully created contact ...'
+    });
+});
+
+
+module.exports = router;
